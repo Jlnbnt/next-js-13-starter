d10e092b1085d6d3511958f9b973303fa22d1929
Jlnbnt <benat.julien@gmail.com>
2023-10-30 09:00:25 +0100

    add comments

--- src/types/VoidReturn.type.ts
+++ src/types/VoidReturn.type.ts
@@ -1,1 +1,6 @@
-export type VoidReturn<T = void> = T extends void ? () => void : (arg: T) => void
+/**
+ * Type for functions that return void, with or without an argument
+ */
+export type VoidReturn<T = void> = T extends void
+  ? () => void
+  : (arg: T) => void
